13496d11a2aa3108b198aa769b1c1838fd205f21
BradenFish <braden.j.fisher-1@ou.edu>
2023-11-06 08:00:33 -0600

    Add project files.

---
+++ MIS3033_LC_1106_Braden Fisher/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MIS3033_LC_1106_Braden_Fisher.Controllers
+{
+    public class HomeController : Controller// name is Home
+    {
+        public int GetInt()
+        {
+            return 20;
+        }
+
+        public IActionResult Index()// this is called action
+        {
+            return View();
+        }
+    }
+}
