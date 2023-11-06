f3356cf238d75dba888823fb2eee46c7f6bf1f9a
BradenFish <braden.j.fisher-1@ou.edu>
2023-11-06 11:38:23 -0600

    post lecture code 11/06

---
+++ MIS3033_LC_1106_Braden Fisher/Controllers/AController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MIS3033_LC_1106_Braden_Fisher.Controllers
+{
+    public class AController : Controller// inherit, name is "A"
+    {
+        public IActionResult B()
+        {
+            return View();
+        }
+
+        public JsonResult GetR()// function, action, web api
+        {
+            var r = new { ID = "S123", Name = "Braden Fisher", Grade = 96.6 }; 
+            return Json(r);
+        }
+        public IActionResult Index()// action, view
+        {
+            ViewBag.stu = new { ID = "S123", Name = "Braden Fisher", Grade = 96.6 };
+            ViewBag.age = 20;
+            
+            return View();
+        }
+    }
+}
